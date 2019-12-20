8845a93f7fc57ec422b871e8010f4dc16ce6066b
CHRoosipuu <carl@chroosipuu.com>
2019-12-20 18:07:54 +0200

    Initial commit

---
+++ Best Guess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Best Guess
+//
+//  Created by Carl Henry Roosipuu on 12/20/19.
+//  Copyright © 2019 CHRoosipuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
