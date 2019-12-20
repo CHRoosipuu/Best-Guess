a222ca586a71a2808ab545c3e1c8f30228559830
CHRoosipuu <carl@chroosipuu.com>
2019-12-20 18:42:06 +0200

    Basic project structure setup

---
+++ Best Guess/ViewControllers/TakeAGuessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TakeAGuessViewController.swift
+//  Best Guess
+//
+//  Created by Carl Henry Roosipuu on 12/20/19.
+//  Copyright © 2019 CHRoosipuu. All rights reserved.
+//
+
+import UIKit
+
+class TakeAGuessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
